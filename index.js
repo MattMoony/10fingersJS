b16e15794bf4b73e9ec38e85687c609b3ccee5d3
MattMoony <monschein.matthias@gmail.com>
2019-03-09 22:05:14 +0100

    added further examples

--- index.js
+++ index.js
@@ -5,18 +5,21 @@ let target = document.getElementById('target'),
     typer = new TenFingers(target);
 
 
-// typer .clear()
-//       .type('Hello World!')
-//       .pause(2000)
-//       .delete(' World!'.length)
-//       .type(' everyone!')
-//       .pause(1000)
-//       .clear()
-//       .type('This is a statement')
-//       .pauseS(1)
-//       .type('.');
+typer .clear()
+      .type('Hello World!')
+      .pause(2000)
+      .delete(' World!'.length)
+      .type(' everyone!')
+      .pause(1000)
+      .clear()
+      .type('This is a statement')
+      .pauseS(1)
+      .type('.');
 
-typer.typeAll([
+let target2 = document.getElementById('target2'),
+    typer2 = new TenFingers(target2);
+
+typer2.typeAll([
   '!&copy; MattMoony',
   'Hello World!',
   'Hello Whales!',
@@ -25,4 +28,23 @@ typer.typeAll([
   'Hello Mum!',
   'This is a statement',
   'This is a statement.',
+]);
+
+let target3 = document.getElementById('target3'),
+    typer3 = new TenFingers(target3, {
+      typingSpeed: 250,
+      deletingSpeed: 125,
+      cursorSpeed: .25,
+      pauseTimeout: 1000,
+      // pauseTimeoutS: 1,
+      endTimeout: 3000,
+      endStringTimeout: 1000,
+      loop: true,
+    });
+
+typer3.typeAll([
+  'Hello World!',
+  'How are you doing?',
+  'How are the people around you doing?',
+  'Great, it was nice talking to you!',
 ]);
